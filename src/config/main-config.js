8dc2d260427bd351cfdd7c075d48d96b542911e0
Jason Chavez <jasonc1887@gmail.com>
2019-05-03 16:49:12 -0600

    WIP deploy to production

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-
+const express = require('express');
+const app = express();
 const logger = require('morgan');
 
 module.exports = {
     init(){
-        app.use(logger('dev'));
+       app.use(logger('dev'));
     }
 };
