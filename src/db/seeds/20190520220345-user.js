64351b5443471c745b121deea517c36335da571e
Jason Chavez <jasonc1887@gmail.com>
2019-05-20 16:29:10 -0600

    blocipedia checkpoint 7: completed

---
+++ src/db/seeds/20190520220345-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const faker = require("faker");
+
+let users = [];
+
+for(let i = 1 ; i <= 10; i++){
+  users.push({
+    email: faker.internet.email(),
+    password: faker.hacker.noun(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Users', users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('Users', null, {});
+  }
+};
